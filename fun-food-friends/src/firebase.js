9e0c4001506c67085c19fd83e5ba31d23b6452df
jl31150 <nafrayo@gmail.com>
2017-06-14 15:36:44 +0200

    init tp

---
+++ fun-food-friends/src/firebase.js
@@ -0,0 +1,12 @@
+
+import firebase from 'firebase'
+const config = {
+  apiKey: "AIzaSyAC82RUy7xmPr_CEqcgJ3N7IdpVtp_jjdU",
+  authDomain: "fun-food-friends-36f99.firebaseapp.com",
+  databaseURL: "https://fun-food-friends-36f99.firebaseio.com",
+  projectId: "fun-food-friends-36f99",
+  storageBucket: "fun-food-friends-36f99.appspot.com",
+  messagingSenderId: "63410891575"
+};
+firebase.initializeApp(config);
+export default firebase;
